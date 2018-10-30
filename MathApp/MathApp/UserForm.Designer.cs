6b3925ce0446eba8d9a7fb64ff4f5091295a7f17
Patrick Leishman <kodiaklifts@gmail.com>
2018-10-29 18:46:00 -0600

    First Commit.

---
+++ MathApp/MathApp/UserForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MathApp
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.ageLbl = new System.Windows.Forms.Label();
+            this.nameTxtBox = new System.Windows.Forms.TextBox();
+            this.ageTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(105, 130);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(38, 13);
+            this.nameLbl.TabIndex = 0;
+            this.nameLbl.Text = "Name:";
+            // 
+            // ageLbl
+            // 
+            this.ageLbl.AutoSize = true;
+            this.ageLbl.Location = new System.Drawing.Point(149, 185);
+            this.ageLbl.Name = "ageLbl";
+            this.ageLbl.Size = new System.Drawing.Size(29, 13);
+            this.ageLbl.TabIndex = 1;
+            this.ageLbl.Text = "Age:";
+            // 
+            // nameTxtBox
+            // 
+            this.nameTxtBox.Location = new System.Drawing.Point(152, 127);
+            this.nameTxtBox.Name = "nameTxtBox";
+            this.nameTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTxtBox.TabIndex = 2;
+            // 
+            // ageTxtBox
+            // 
+            this.ageTxtBox.Location = new System.Drawing.Point(185, 177);
+            this.ageTxtBox.Name = "ageTxtBox";
+            this.ageTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.ageTxtBox.TabIndex = 3;
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 462);
+            this.Controls.Add(this.ageTxtBox);
+            this.Controls.Add(this.nameTxtBox);
+            this.Controls.Add(this.ageLbl);
+            this.Controls.Add(this.nameLbl);
+            this.Name = "UserForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UserForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label ageLbl;
+        private System.Windows.Forms.TextBox nameTxtBox;
+        private System.Windows.Forms.TextBox ageTxtBox;
+    }
+}
